164ce806ee2c81413d603920b9b1498d1a7b3288
Nikeshkumar-tk <104181447+Nikeshkumar-tk@users.noreply.github.com>
2022-10-27 19:45:08 +0530

    Navbar completed

---
+++ components/Navbar/Navbar.tsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import { User } from '../../icons'
+import styles from './Navbar.module.css'
+
+
+
+const Navbar = () => {
+    const [mobileMenu, setMobileMenu] = useState<boolean>(false)
+
+    return (
+        <div className={styles["nav-wrap"]}>
+
+            <div className={styles.container}>
+
+                <h3 className={styles.logo}>Learn Dev</h3>
+                <ul className={styles["nav-ul"]}>
+                    <li>Home</li>
+                    <li>Browse</li>
+                    <li>Roadmaps</li>
+                    <li>Cheatsheets</li>
+                </ul>
+                <div className={styles["icon-wrap"]}>
+
+                    <User />
+                    <div
+                     className={`${styles["handburger-wrap"]} ${mobileMenu ? styles.active : ""}`}
+                     onClick = {() => {setMobileMenu(p => !p)}}
+                     
+                     >
+                        <div className={`${styles.bar} ${styles["bar-1"]}`}></div>
+                        <div className={`${styles.bar} ${styles["bar-2"]}`}></div>
+                        <div className={`${styles.bar} ${styles["bar-3"]}`}></div>
+                    </div>
+                </div>
+
+            </div>
+            <span className={styles["mobile-wrap"]}></span>
+        </div>
+    )
+}
+
+export default Navbar
